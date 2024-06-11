a224b3092b4188d5d46093fdc4ffe5dbce39b61c
Nguyen Duc Nhat <4601104127@student.hcmue.edu.vn>
2024-06-11 15:17:55 +0700

    Update [User] Controller (format base64)

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -2,25 +2,25 @@ import { Post, Body, Get, Patch, Controller, Query } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogDTO } from 'src/database/dto/blog.dto';
 
-@Controller('Blog')
+@Controller('blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
-  @Post()
+  @Post('create')
   createBlog(@Body() blog: BlogDTO) {
     return this.blogService.createBlog(blog);
   }
 
-  @Get()
+  @Get('getbytype')
   getBlogsByType(@Query('blog_type') blog_type: string) {
     return this.blogService.getBlogsByType(blog_type);
   }
 
-  @Get()
+  @Get('getbyblog')
   getBlogById(@Query('blog_id') blog_id: string) {
     return this.blogService.getBlogById(blog_id);
   }
 
-  @Patch()
+  @Patch('update')
   updateBlog(@Query('blog_id') blog_id: string, @Body() blog: BlogDTO) {
     return this.blogService.updateBlog(blog_id, blog);
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,19 +3,22 @@ import { UserService } from './user.service';
 import { CreateUserDto } from 'src/database/dto';
 import { Body, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import mime from 'mime';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('upload-avatar')
+  @Post('update')
   @UseInterceptors(FileInterceptor('avatar'))
   async uploadAvatar(
     @UploadedFile() file: Express.Multer.File,
     @Body() createUserDto: CreateUserDto,
     @Body('user_id') user_id: string,
   ) {
-    const avatarBase64 = file.buffer.toString('base64');
+    const mimeType = mime.lookup(file.originalname);
+    const base64Prefix = `data:${mimeType};base64,`;
+    const avatarBase64 = base64Prefix + file.buffer.toString('base64');
     createUserDto.avatar = avatarBase64;
     return this.userService.updateUser(user_id, createUserDto);
   }
